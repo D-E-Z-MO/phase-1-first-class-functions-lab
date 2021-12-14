147a6bed4b7e7dff9e6be4817ffe63d1a2ab8a97
douglassmoss <douglassmoss@icloud.com>
2021-12-14 09:20:09 -0800

    commit tree ab13607dc9c0167331ecdc11cf8474b9adf3d1f1

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (drivers) => {
+  return drivers.splice(0,2)
+}
+const returnLastTwoDrivers = (drivers) => {
+  return drivers.slice(drivers.length - 2)
+}
+
+let selectingDrivers = [
+  returnFirstTwoDrivers,
+  returnLastTwoDrivers()
+
+];
+
+const createFareMultiplier = (n) => {
+  return () => {
+    n * 4
+  }
+}
+const fareDoubler = () => {
+  createFareMultiplier() * 2;
+}
+const fareTripler = () => {
+  createFareMultiplier() * 3;;
+}
+const selectDifferentDrivers = function(drivers, arrayOfDrivers) {
+  return arrayOfDrivers(drivers)
+}
